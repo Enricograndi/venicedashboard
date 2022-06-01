ee179edfc9e411ec06cd36f50f6eb912ccb14526
Enricograndi <enricograndi14@gmail.com>
2022-06-01 09:48:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -295,7 +295,7 @@ def update_graph_2( radio):
     return venice_map, barchart
     
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True,  host='0.0.0.0', port=8050)
 
 
 
