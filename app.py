f60c14354989e4a2764590db6eaf5485d67e5565
Enricograndi <enricograndi14@gmail.com>
2022-06-01 09:42:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import dash
 
 # For interactive components like graphs, dropdowns, or date ranges.
-from dash import dcc
+from dash import dcc, Dash
 # For HTML tags
 
 #from dash.dependencies import Input, Output
@@ -14,7 +14,6 @@ import pandas as pd
 import plotly.express as px 
 import plotly.graph_objs as go
 
-import dash
 import dash_bootstrap_components as dbc
 from dash import html
 from dash.dependencies import Input, Output
